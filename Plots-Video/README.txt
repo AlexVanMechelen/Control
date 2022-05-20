ef82263107afe9a252147b282718d79ff9acb7ac
AlexVanMechelen <alex.vanmechelen@hotmail.com>
2022-05-20 10:04:32 +0200

    Added slide plots & README for ESSF

---
+++ Plots-Video/README.txt
@@ -0,0 +1,11 @@
+----------------
+SSEF_I_vs_PI.png
+----------------
+De beginwaarde 0.1 rad werd opgelegd voor de hoek en 0 m voor de positie.
+Als stelwaarde werd 1.2 m meegegeven. Zoals verwacht is de Extended State
+Space Feedback met PI net iets sneller dan deze met slechts een I.
+Dit is aangezien de integrator slechts vanaf de tweede tijdstap kan reageren
+op de aangelegde stap.
+
+
+
---
+++ slide_plots.m
@@ -0,0 +1,41 @@
+set(groot,'defaulttextinterpreter','latex');
+set(groot, 'defaultAxesTickLabelInterpreter','latex');
+set(groot, 'defaultLegendInterpreter','latex');
+set(groot,'defaultAxesXGrid','on')
+set(groot,'defaultAxesYGrid','on')
+set(groot,'defaultLineMarkerSize',35)
+set(groot,'defaultAxesFontSize',40)
+set(0,'units','pixels');
+ans = get(0,'ScreenSize');
+set(groot,'defaultFigurePosition',[0 0 ans(3) ans(4)])
+set(groot,'defaultLineLineWidth',2)
+set(groot,'defaultLegendLocation','best')
+set(groot,'defaultAxesGridAlpha',0.5)
+set(groot,'defaultAxesLineWidth',1)
+set(groot,'defaultConstantLineLineWidth',2)
+PATH = pwd;
+%% Relative error
+F = figure;
+x = 0:0.0001*pi:5*pi/4;
+plot(x,abs(x-sin(x))./sin(x),x,abs(1-cos(x))./cos(x));
+xlim([0 5*pi/4])
+k = 10^2
+ylim([-k k])
+xticks(0:pi/4:5*pi/4)
+xticklabels({'0','$\pi$/4','$\pi$/2','3$\pi$/4','$\pi$', '5$\pi$/4'})
+title("Relative error small angle hypothesis")
+ylabel("Relative error [-]")
+xlabel("Angle [rad]")
+exportgraphics(F,PATH+"/Plots-Video/Small_angle_rel_err.png",'Resolution',300)
+%% Absolute error
+F = figure;
+x = 0:0.0001*pi:5*pi/4;
+plot(x,abs(x-sin(x)),x,abs(1-cos(x)));
+xlim([0 5*pi/4])
+ylim([0 5])
+xticks(0:pi/4:5*pi/4)
+xticklabels({'0','$\pi$/4','$\pi$/2','3$\pi$/4','$\pi$', '5$\pi$/4'})
+title("Absolute error small angle hypothesis")
+ylabel("Absolute error [-]")
+xlabel("Angle [rad]")
+exportgraphics(F,PATH+"/Plots-Video/Small_angle_abs_err.png",'Resolution',300)
